0211792a9edee855c64fd00bead9c4511884edfa
Ivan Eroshkin <83237781+iverosh@users.noreply.github.com>
2023-11-23 19:55:00 +0300

    Added incremental euclidian distance
    Also fixed Batch Mds

--- BatchMDS.py
+++ BatchMDS.py
@@ -1,35 +1,35 @@
 import numpy as np
 import pandas as pd
-import os
 import matplotlib.pyplot as plt
-from Toolbox import two_d_eq, Assign_features_to_pixels
+from Toolbox import two_d_eq
 from sklearn.manifold import MDS
 from sklearn.metrics.pairwise import euclidean_distances
 import math
-from itertools import product
-import paraHill
-from Toolbox import REFINED_Im_Gen
-from scipy.spatial.distance import euclidean
-import pprint
-nn = 100
-
-Euc_Dist = np.zeros(shape=(nn, nn))
-for i in range(nn):
-    x = pd.read_csv("data/normalized_padel_feats_NCI60_672_small.csv", usecols=[i])
-    x = x.iloc[:, 0].to_list()
-    for j in range(nn):
-        if i <= j:
-            continue
-        y = pd.read_csv("data/normalized_padel_feats_NCI60_672_small.csv", usecols=[j])
-        y = y.iloc[:, 0].to_list()
-        Euc_Dist[i][j] = euclidean(x, y)
-Euc_Dist = np.maximum(Euc_Dist, Euc_Dist.T)
+n = 20 # количество колонок считанных за один раз
+readed_n = 0
+total_n = 100
+mds_xy = np.array([0])
 embedding = MDS(n_components=2)										 # Reduce the dimensionality by MDS into 2 components
-mds_xy = embedding.fit_transform(Euc_Dist)					         # Apply MDS
-eq_xy = two_d_eq(mds_xy,nn) # -> [0,1]
+while readed_n < total_n: # нужно добавить случай если не кратно
 
+    readed_n += n
+    Feat_DF = pd.read_csv("data/normalized_padel_feats_NCI60_672_small.csv", usecols=range(readed_n, readed_n+n))     #"data/normalized_padel_feats_NCI60_672_small.csv"
+    X = Feat_DF.values                          
+    original_input = pd.DataFrame(data = X)
 
+    feature_names_list = Feat_DF.columns.tolist()
+    nn = math.ceil(np.sqrt(len(feature_names_list)))      			     # Image dimension
+    Nn = original_input.shape[1]                                         # Number of features
+    transposed_input = original_input.T 							     # The MDS input data must be transposed , because we want summarize each feature by two values (as compard to regular dimensionality reduction each sample will be described by two values)
+    Euc_Dist = euclidean_distances(transposed_input) 					 # Euclidean distance
+    Euc_Dist = np.maximum(Euc_Dist, Euc_Dist.transpose())   			 # Making the Euclidean distance matrix symmetric
+    
+    if len(mds_xy) == 1:
+        mds_xy = embedding.fit_transform(transposed_input)					 # Apply MDS
+    else:
+        mds_xy = np.vstack((mds_xy, embedding.fit_transform(transposed_input)))
 
+eq_xy = two_d_eq(mds_xy,total_n) # -> [0,1]
 
 fig, ax = plt.subplots(1, 2)
 
@@ -47,9 +47,8 @@ Nn = original_input.shape[1]                                         # Number of
 transposed_input = original_input.T 							     # The MDS input data must be transposed , because we want summarize each feature by two values (as compard to regular dimensionality reduction each sample will be described by two values)
 Euc_Dist = euclidean_distances(transposed_input) 					 # Euclidean distance
 Euc_Dist = np.maximum(Euc_Dist, Euc_Dist.transpose())   			 # Making the Euclidean distance matrix symmetric
-embedding = MDS(n_components=2)										 # Reduce the dimensionality by MDS into 2 components
 mds_xy = embedding.fit_transform(transposed_input)					 # Apply MDS
-print(np.allclose(transposed_input,transposed_input.T))
+
 
 eq_xy = two_d_eq(mds_xy,Nn) # -> [0,1]
 
